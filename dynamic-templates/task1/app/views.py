e188b925c53a77efab70048af5248b33f9bb6b76
Nichi13 <47666044+Nichi13@users.noreply.github.com>
2019-06-17 23:37:13 +0300

    Add files via upload

---
+++ dynamic-templates/task1/app/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.conf import settings
+import csv
+import os
+
+
+def inflation_view(request):
+    template_name = 'inflation.html'
+    path = os.path.join(settings.BASE_DIR)
+
+    with open(('%s\\inflation_russia.csv' % path), encoding='utf-8', newline='') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter =';')
+        file_list = []
+        for item in reader:
+            keys = item.keys()
+            file_list.append(item)
+    data = {"message": file_list, 'keys':keys}
+
+    context = data
+
+    return render(request, template_name,
+                  context)
+
---
+++ dynamic-templates/task3/app/templatetags/news_filters.py
@@ -0,0 +1,53 @@
+from django import template
+from datetime import datetime
+
+register = template.Library()
+
+
+@register.filter
+def format_date(value):
+    date = datetime.fromtimestamp(value)
+    now = datetime.now()
+    delta = now - date
+    delta_minutes= delta.seconds / 60
+    hours = int(delta_minutes / 60)
+    format_date = datetime.fromtimestamp(value).strftime("%Y-%m-%d")
+    if delta_minutes < 10:
+        result = 'только что'
+    elif hours < 1:
+        result = '%s минут назад' %( int(delta_minutes))
+    elif hours < 24:
+        result = '%s часов назад' %(hours)
+    elif hours >= 24:
+        result = format_date
+    return result
+
+# необходимо добавить фильтр для поля `score`
+@register.filter
+def format_num_comments(value):
+    if value == 0:
+        result = 'Оставьте коментарий'
+    elif 0 < value <= 50:
+        result = value
+    elif 50 < value:
+        result = '50+'
+    return result
+
+
+@register.filter
+def score(value):
+    if value < -5:
+        result = 'все плохо'
+    elif  -5 <= value <= 5 :
+        result = 'нейтрально'
+    elif 5 < value:
+        result = 'хорошо'
+    return result
+
+
+@register.filter
+def format_selftext(value, count):
+    text_len = len(value)
+    result = value[0:count]+'...'+value[(text_len-count):(text_len)]
+    return result
+
